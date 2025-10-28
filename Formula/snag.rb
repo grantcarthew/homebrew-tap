218a6eaa3bb384d164ace6e5c3b723e0283d8da1
Grant Carthew <grant.carthew@autogeneral.com.au>
2025-10-28 16:28:07 +1000

    chore(formula): update snag version to 0.0.4

--- Formula/snag.rb
+++ Formula/snag.rb
@@ -2,7 +2,7 @@ class Snag < Formula
   desc "Intelligently fetch web pages using Chrome via CDP"
   homepage "https://github.com/grantcarthew/snag"
   url "https://github.com/grantcarthew/snag/archive/refs/tags/v0.0.4.tar.gz"
-  sha256 "2dd03ab153492a34f408607aa1b95a871953179aa192cf9498cf311ee9d50456"
+  sha256 "ed81ec9763530c0bd2dcc91d386526aa8451f35115346f1c674ce2afa71542dd"
   license "MPL-2.0"
 
   depends_on "go" => :build
