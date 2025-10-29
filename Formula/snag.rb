b603915ce98281bc537b4d8ff93803c4a44cb055
grantcarthew <grant@carthew.net>
2025-10-29 20:47:39 +1000

    chore(snag): update to v0.0.5

--- Formula/snag.rb
+++ Formula/snag.rb
@@ -1,18 +1,18 @@
 class Snag < Formula
-  desc "Intelligently fetch web pages using Chrome via CDP"
+  desc "Intelligently fetch web page content using a browser engine"
   homepage "https://github.com/grantcarthew/snag"
-  url "https://github.com/grantcarthew/snag/archive/refs/tags/v0.0.4.tar.gz"
-  sha256 "ed81ec9763530c0bd2dcc91d386526aa8451f35115346f1c674ce2afa71542dd"
+  url "https://github.com/grantcarthew/snag/archive/refs/tags/v0.0.5.tar.gz"
+  sha256 "38cb1de9d4092179f1c800aeab8688ec88c8b7444de426d5f67da8ab36029e90"
   license "MPL-2.0"
 
   depends_on "go" => :build
 
   def install
     ENV["CGO_ENABLED"] = "0"
-    system "go", "build", *std_go_args(ldflags: "-X main.version=0.0.4", output: bin/"snag")
+    system "go", "build", *std_go_args(ldflags: "-X main.version=0.0.5", output: bin/"snag")
   end
 
   test do
-    assert_match "0.0.4", shell_output("#{bin}/snag --version")
+    assert_match "0.0.5", shell_output("#{bin}/snag --version")
   end
 end
