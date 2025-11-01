68ea4f352dae2c75ba0e895ff7ed04cf0431ab72
Grant Carthew <grant@carthew.net>
2025-11-01 20:44:01 +1000

    build(Formula): update to kagi v1.0.0

--- Formula/kagi.rb
+++ Formula/kagi.rb
@@ -1,17 +1,17 @@
 class Kagi < Formula
   desc "CLI tool for querying Kagi FastGPT API"
   homepage "https://github.com/grantcarthew/kagi"
-  url "https://github.com/grantcarthew/kagi/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "6e6eb9f558527c6421902fb4c36c3fa2a1f061170ba5811e141159fdbd6d16f7"
+  url "https://github.com/grantcarthew/kagi/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "60980c4ced27b80281d8997f96979252331e70ff71698b3ef93e0c2dd01956da"
   license "MPL-2.0"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-X main.version=0.0.1")
+    system "go", "build", *std_go_args(ldflags: "-X main.version=1.0.0")
   end
 
   test do
-    assert_match "0.0.1", shell_output("#{bin}/kagi --version")
+    assert_match "1.0.0", shell_output("#{bin}/kagi --version")
   end
 end
