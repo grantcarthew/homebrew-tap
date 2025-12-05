4c451aa57bdae684723c5d5aab3c7744b30b365b
Grant Carthew <grant@carthew.net>
2025-12-05 16:24:51 +1000

    chore(acon): update to v1.2.0

--- Formula/acon.rb
+++ Formula/acon.rb
@@ -1,18 +1,18 @@
 class Acon < Formula
   desc "CLI for Confluence - because the web editor is not it"
   homepage "https://github.com/grantcarthew/acon"
-  url "https://github.com/grantcarthew/acon/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "33b3d06f2b4c5e25df948c8389a66aa95825f841e8a310111c87ed36cb45bab8"
+  url "https://github.com/grantcarthew/acon/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "4c171b7e59da78beb4ef16b9ca2e936d354b621369291e56e52e1b494b80e3b4"
   license "MPL-2.0"
 
   depends_on "go" => :build
 
   def install
     ENV["CGO_ENABLED"] = "0"
-    system "go", "build", *std_go_args(ldflags: "-X main.version=1.1.0", output: bin/"acon")
+    system "go", "build", *std_go_args(ldflags: "-X main.version=1.2.0", output: bin/"acon")
   end
 
   test do
-    assert_match "1.1.0", shell_output("#{bin}/acon --version")
+    assert_match "1.2.0", shell_output("#{bin}/acon --version")
   end
 end
