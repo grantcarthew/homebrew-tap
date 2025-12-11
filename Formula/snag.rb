898c7e0a095b474e7955ac4a54acc76744011350
grantcarthew <grant@carthew.net>
2025-12-11 20:28:18 +1000

    chore(deps): update snag formula to v1.0.2

--- Formula/snag.rb
+++ Formula/snag.rb
@@ -1,18 +1,18 @@
 class Snag < Formula
   desc "Intelligently fetch web page content using a browser engine"
   homepage "https://github.com/grantcarthew/snag"
-  url "https://github.com/grantcarthew/snag/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "12d431176672d3a374bed9eecb125a1a5014d63917318c8621df0f3607fb12e2"
+  url "https://github.com/grantcarthew/snag/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "d773bc704a69b37a7cbd9f84e6871f6c90b635cfbc57247a9294be16474bb034"
   license "MPL-2.0"
 
   depends_on "go" => :build
 
   def install
     ENV["CGO_ENABLED"] = "0"
-    system "go", "build", *std_go_args(ldflags: "-X main.version=1.0.1", output: bin/"snag")
+    system "go", "build", *std_go_args(ldflags: "-X main.version=1.0.2", output: bin/"snag")
   end
 
   test do
-    assert_match "1.0.1", shell_output("#{bin}/snag --version")
+    assert_match "1.0.2", shell_output("#{bin}/snag --version")
   end
 end
