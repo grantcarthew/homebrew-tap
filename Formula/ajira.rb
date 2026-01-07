5954ce23a0c7216b8f65bc4b3ee083a6ec6c522e
Grant Carthew <grant@carthew.net>
2026-01-07 15:21:56 +1000

    build(ajira): update formula sha256

--- Formula/ajira.rb
+++ Formula/ajira.rb
@@ -2,7 +2,7 @@ class Ajira < Formula
   desc "Jira from your terminal - because browser tabs are overrated"
   homepage "https://github.com/grantcarthew/ajira"
   url "https://github.com/grantcarthew/ajira/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "3a43e3ca3fbcfeb645c7177645525a060550bc8632c28eb86f45bcde0cacd50b"
+  sha256 "96189279e7b6b448c3de30dcee507ef3612444f45125437cba0f09c3781c24b1"
   license "MPL-2.0"
 
   depends_on "go" => :build
