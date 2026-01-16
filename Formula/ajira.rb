10da22be1ef2161ca4603fd4ed0921809de8b3b6
grantcarthew <grant@carthew.net>
2026-01-16 19:39:10 +1000

    ajira: update to 0.2.0

--- Formula/ajira.rb
+++ Formula/ajira.rb
@@ -1,8 +1,8 @@
 class Ajira < Formula
   desc "Jira from your terminal - because browser tabs are overrated"
   homepage "https://github.com/grantcarthew/ajira"
-  url "https://github.com/grantcarthew/ajira/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "fc085b1963ceb73595888232498a44d964574da6229726e40c453ea88e309f7c"
+  url "https://github.com/grantcarthew/ajira/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "7303e1d4edcd79324700590c33351639f12c466e5be993ecc21159c147829301"
   license "MPL-2.0"
 
   depends_on "go" => :build
@@ -14,6 +14,6 @@ class Ajira < Formula
   end
 
   test do
-    assert_match "0.1.0", shell_output("#{bin}/ajira --version")
+    assert_match "0.2.0", shell_output("#{bin}/ajira --version")
   end
 end
