d6141c5af544152efe82c13da98f972a4e284e71
grantcarthew <grant@carthew.net>
2026-01-20 19:19:02 +1000

    build(formula): update start to v0.0.3
    update license from MIT to MPL-2.0

--- Formula/start.rb
+++ Formula/start.rb
@@ -1,9 +1,9 @@
 class Start < Formula
   desc "AI agent CLI orchestrator built on CUE"
   homepage "https://github.com/grantcarthew/start"
-  url "https://github.com/grantcarthew/start/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "c4b3a38cfb81ef94f7112b96c90a7824802af3136325e712c8a1ec5ec8facc10"
-  license "MIT"
+  url "https://github.com/grantcarthew/start/archive/refs/tags/v0.0.3.tar.gz"
+  sha256 "cbde070fb2b664060c570f3ac86eb737c0878867cd8c1c33d35988ed142f6be8"
+  license "MPL-2.0"
 
   depends_on "go" => :build
 
@@ -14,6 +14,6 @@ class Start < Formula
   end
 
   test do
-    assert_match "0.0.2", shell_output("#{bin}/start --version")
+    assert_match "0.0.3", shell_output("#{bin}/start --version")
   end
 end
