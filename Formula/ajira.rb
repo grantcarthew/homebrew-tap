32923d54d57d1beaab9521446c5493bbabec73f1
grantcarthew <grant@carthew.net>
2026-01-28 23:15:27 +1000

    chore(ajira): update to v0.4.0

--- Formula/ajira.rb
+++ Formula/ajira.rb
@@ -1,8 +1,8 @@
 class Ajira < Formula
   desc "Jira from your terminal - because browser tabs are overrated"
   homepage "https://github.com/grantcarthew/ajira"
-  url "https://github.com/grantcarthew/ajira/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "429f0be1c3dc9546de76d5ca8a939d2fb8320f0417e5185414ba95a664dc1d7b"
+  url "https://github.com/grantcarthew/ajira/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "0688ba0b8450d1eecbd492a975afbd1853ef9d4b00e4908764c40573cb52b2da"
   license "MPL-2.0"
 
   depends_on "go" => :build
@@ -14,6 +14,6 @@ class Ajira < Formula
   end
 
   test do
-    assert_match "0.3.0", shell_output("#{bin}/ajira --version")
+    assert_match "0.4.0", shell_output("#{bin}/ajira --version")
   end
 end
