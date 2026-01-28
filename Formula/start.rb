779021768a1d8cb270c7ac9561ac19c6e53f497d
Grant Carthew <grant@carthew.net>
2026-01-28 23:19:18 +1000

    chore(deps): update start formula to v0.1.0

--- Formula/start.rb
+++ Formula/start.rb
@@ -1,8 +1,8 @@
 class Start < Formula
   desc "AI agent CLI orchestrator built on CUE"
   homepage "https://github.com/grantcarthew/start"
-  url "https://github.com/grantcarthew/start/archive/refs/tags/v0.0.4.tar.gz"
-  sha256 "e05dcf4df99729aeac54520077c95087122b27420ff99f1ab3304f8b2ae42628"
+  url "https://github.com/grantcarthew/start/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "e5d76b7570a4cb79baffeff4927e3f462324d8e73c151620acf433bf495c42d2"
   license "MPL-2.0"
 
   depends_on "go" => :build
@@ -14,6 +14,6 @@ class Start < Formula
   end
 
   test do
-    assert_match "0.0.4", shell_output("#{bin}/start --version")
+    assert_match "0.1.0", shell_output("#{bin}/start --version")
   end
 end
