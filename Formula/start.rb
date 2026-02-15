46b472dce45ffbf3422bf2ca318ddd5a102b860c
grantcarthew <grant@carthew.net>
2026-02-15 23:40:38 +1000

    chore(deps): update start formula to v0.6.0

--- Formula/start.rb
+++ Formula/start.rb
@@ -1,19 +1,20 @@
 class Start < Formula
   desc "AI agent CLI orchestrator built on CUE"
   homepage "https://github.com/grantcarthew/start"
-  url "https://github.com/grantcarthew/start/archive/refs/tags/v0.5.1.tar.gz"
-  sha256 "32d8106b5f98f6dc06ef0382a250f0d959682558badca069c0eb31781312e446"
+  url "https://github.com/grantcarthew/start/archive/refs/tags/v0.6.0.tar.gz"
+  sha256 "d0db354a33bf6f95de0eb7bbb82976addb0f7423875bfd7bfb22cf9664b11e2f"
   license "MPL-2.0"
 
   depends_on "go" => :build
 
   def install
     ENV["CGO_ENABLED"] = "0"
-    ldflags = "-s -w -X github.com/grantcarthew/start/internal/cli.cliVersion=#{version}"
+    pkg = "github.com/grantcarthew/start/internal/cli"
+    ldflags = "-s -w -X #{pkg}.cliVersion=#{version} -X #{pkg}.commit=#{Utils.git_head} -X #{pkg}.buildDate=#{time.iso8601}"
     system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/start"
   end
 
   test do
-    assert_match "0.5.1", shell_output("#{bin}/start --version")
+    assert_match "0.6.0", shell_output("#{bin}/start --version")
   end
 end
