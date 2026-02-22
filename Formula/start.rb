409ca4826e8c96f030fdc1a6849b0f044e2c3c11
Grant Carthew <grant@carthew.net>
2026-02-22 16:54:06 +1000

    start: update to 0.7.0

--- Formula/start.rb
+++ Formula/start.rb
@@ -1,8 +1,8 @@
 class Start < Formula
   desc "AI agent CLI orchestrator built on CUE"
   homepage "https://github.com/grantcarthew/start"
-  url "https://github.com/grantcarthew/start/archive/refs/tags/v0.6.0.tar.gz"
-  sha256 "d0db354a33bf6f95de0eb7bbb82976addb0f7423875bfd7bfb22cf9664b11e2f"
+  url "https://github.com/grantcarthew/start/archive/refs/tags/v0.7.0.tar.gz"
+  sha256 "4447197cfa2b495848894391cd9f089e2c0216fb2c96397c7e5be3d133fdd4d5"
   license "MPL-2.0"
 
   depends_on "go" => :build
@@ -10,12 +10,12 @@ class Start < Formula
   def install
     ENV["CGO_ENABLED"] = "0"
     pkg = "github.com/grantcarthew/start/internal/cli"
-    commit = "4624a8c06a0ac93bdc3a1d02d2e63abeb675d5f6"
+    commit = "490adffb4c6d5fa1469bb1bf30b80ed27b0e8732"
     ldflags = "-s -w -X #{pkg}.cliVersion=#{version} -X #{pkg}.commit=#{commit} -X #{pkg}.buildDate=#{time.iso8601}"
     system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/start"
   end
 
   test do
-    assert_match "0.6.0", shell_output("#{bin}/start --version")
+    assert_match "0.7.0", shell_output("#{bin}/start --version")
   end
 end
