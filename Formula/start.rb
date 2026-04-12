8e853c55243d051917a14821b049a1653add8400
Grant Carthew <grant@carthew.net>
2026-04-12 12:03:24 +1000

    start: update to 0.9.3

--- Formula/start.rb
+++ Formula/start.rb
@@ -1,8 +1,8 @@
 class Start < Formula
   desc "AI agent CLI orchestrator built on CUE"
   homepage "https://github.com/grantcarthew/start"
-  url "https://github.com/grantcarthew/start/archive/refs/tags/v0.9.2.tar.gz"
-  sha256 "11410ebe616b54e700b177a39b0ffb334db4c1074a38094ed51f2ff25209f08f"
+  url "https://github.com/grantcarthew/start/archive/refs/tags/v0.9.3.tar.gz"
+  sha256 "6360ee4928abcc4c2fee786387f44feb4edd63e1b19a80eb378d1d6326e343f4"
   license "MPL-2.0"
 
   depends_on "go" => :build
@@ -10,12 +10,12 @@ class Start < Formula
   def install
     ENV["CGO_ENABLED"] = "0"
     pkg = "github.com/grantcarthew/start/internal/cli"
-    commit = "cab34f562232346339a054fa2b9068baef2e0b25"
+    commit = "3ffb2907caf2ce933f7aa5264f361f2755079cc3"
     ldflags = "-s -w -X #{pkg}.cliVersion=#{version} -X #{pkg}.commit=#{commit} -X #{pkg}.buildDate=#{time.iso8601}"
     system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/start"
   end
 
   test do
-    assert_match "0.9.2", shell_output("#{bin}/start --version")
+    assert_match "0.9.3", shell_output("#{bin}/start --version")
   end
 end
