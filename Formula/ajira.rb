ad3d4f53b39b83047ca36b5f9264329a2eb8ce18
Grant Carthew <grant@carthew.net>
2026-04-23 19:13:49 +1000

    ajira: update to 1.0.0

--- Formula/ajira.rb
+++ Formula/ajira.rb
@@ -1,19 +1,19 @@
 class Ajira < Formula
   desc "Jira from your terminal - because browser tabs are overrated"
   homepage "https://github.com/grantcarthew/ajira"
-  url "https://github.com/grantcarthew/ajira/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "0688ba0b8450d1eecbd492a975afbd1853ef9d4b00e4908764c40573cb52b2da"
+  url "https://github.com/grantcarthew/ajira/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "f473b2401c6bacd583a98c3955287fe16007496cc5654c28e5707431595fe99f"
   license "MPL-2.0"
 
   depends_on "go" => :build
 
   def install
     ENV["CGO_ENABLED"] = "0"
-    ldflags = "-s -w -X github.com/gcarthew/ajira/internal/cli.Version=#{version}"
+    ldflags = "-s -w -X github.com/grantcarthew/ajira/internal/cli.Version=#{version}"
     system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/ajira"
   end
 
   test do
-    assert_match "0.4.0", shell_output("#{bin}/ajira --version")
+    assert_match "1.0.0", shell_output("#{bin}/ajira --version")
   end
 end
